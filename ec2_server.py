0d499f239f29a0a0a82a4d9914daf53e4d31fb80
mtcnxd <mtc.nxd@gmail.com>
2026-04-22 11:41:20 -0600

    Ignore

---
+++ ec2_server.py
@@ -0,0 +1,19 @@
+import boto3
+from Services.configurations import instance_id, aws_access_key_id, aws_secret_access_key
+
+ec2 = boto3.client(
+    'ec2', 
+    region_name='us-east-1',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
+
+try:
+    response = ec2.start_instances(InstanceIds=[aws_instance_id])
+
+    ec2.stop_instances(InstanceIds=[aws_instance_id])
+
+    print("Instancia iniciada correctamente")
+
+except Exception as e:
+    print(e)
